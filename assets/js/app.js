1def6b24554b5ed75bcb2b68839a4750d1194280
gustavI0 <gus.guyon@gmail.com>
2020-08-05 10:16:04 +0200

    added bootstrap via webpack

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,10 @@
 import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import $ from 'jquery';
+
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
